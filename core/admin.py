d7c9e2f0fc1b0a848e31e168e13e56c29a78abb2
MarceloCFSF <marcelo.cfsf@gmail.com>
2024-10-16 15:01:54 -0300

    feat: add business rules to video model

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from core.models import Video
 from core.models import Tag
 
+class VideoAdmin(admin.ModelAdmin):
+  readonly_fields = ('published_at', 'num_likes', 'num_views', 'author')
+  list_display = ('title', 'is_published', 'published_at', 'num_likes', 'num_views')
+  search_fields = ('title', 'description', 'tags__name')
+  list_filter = ('is_published', 'tags')
+  date_hierarchy = 'published_at'
+
+  def save_model(self, request, obj, form, change):
+    if not obj.pk:
+      obj.author = request.user
+
+    return super().save_model(request, obj, form, change)
+
 # Register your models here.
-admin.site.register(Video)
+admin.site.register(Video, VideoAdmin)
 admin.site.register(Tag)
